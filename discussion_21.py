45e3a1359d7f51f9cf5f398f386f2bf3b1bcaf7c
SeunghughKim <89321974+SeunghughKim@users.noreply.github.com>
2023-06-12 13:22:06 -0700

    Update discussion_21.py

--- discussion_21.py
+++ discussion_21.py
@@ -8,7 +8,7 @@ uploaded_file = st.file_uploader("Upload a CSV file", type = "csv")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     
-    st,write("Number of rows:", df.shape[0])
+    st.write("Number of rows:", df.shape[0])
     st.write("Number of Columns:", df.shape[1])
     st.write("Number of categorical variables:", len(df.select_dtypes(include='object').columns))
     st.write("Number of numerical variables:", len(df.select_dtypes(include='number').columns))
